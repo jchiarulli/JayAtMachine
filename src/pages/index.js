ad824630c0ef4e12162df6ef0ebcdf61b3882e0f
Jason Chiarulli <jason@J.local>
2020-08-11 21:04:19 -0400

    Set up some basic functionality

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hey everyone!</h1>
+    <p>My name is Jason Chiarulli.</p>
+    <p>Click on a topic that interests you.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  </Layout>
+)
+
+export default IndexPage
